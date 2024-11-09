846176ab56fbe8272e55ab48f0ea3aebbb38f983
Danny <daid7@mcmaster.ca>
2024-11-09 00:40:32 -0500

    Added temporary main clean.c, working make file, moved txt out of folder, testing build.yml

---
+++ clean.c
@@ -0,0 +1,12 @@
+/* Danny Dai, 400505160, 2024/11/21
+ * 
+ */
+
+// Include standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    puts("Hello World");
+    return EXIT_SUCCESS;
+}
